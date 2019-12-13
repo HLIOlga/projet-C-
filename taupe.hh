61075040997aa6e8749faed0a7ad8945f4a13e48
HLI <liheyang0014@gmail.com>
2019-12-13 21:52:33 +0100

    first

---
+++ taupe.hh
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <vector> // taupe de vector
+
+using namespace std;
+
+class Taupe{
+protected:
+  bool _present;
+  const unsigned int _note;
+  
+public:
+  
+};
+
+// << chongzai
