8b273c0cd31cf4218a2daab795afca9ef503c9a9
BaoHuiling <baozii.721@outlook.com>
2019-12-15 14:56:48 +0100

    playbutton finish

--- main.cc
+++ main.cc
@@ -5,11 +5,26 @@
 using namespace std;
 SDL_Window * window = NULL;
 SDL_Renderer *renderer = NULL;
-SDL_Surface *background = NULL;
-SDL_Texture * backtext = NULL;
+SDL_Surface *background = NULL, *playbutton = NULL;
+SDL_Texture * backtext = NULL, *playtext = NULL;
+SDL_Rect PlayRect={357,196,207,126};
 
 void MelangeTaupe(){
   
+}
+
+void Destroy(){
+    SDL_DestroyTexture(backtext);
+    SDL_FreeSurface(background);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+}
+void halt(){
+    cerr<<SDL_GetError()<<endl;
+    Destroy();
+    exit(-1);
 }
 
 bool sys_Init(){
@@ -38,32 +53,48 @@ bool DrawBackGround(){
    return true;
 }
 
+bool PutButton(){
+  DrawBackGround();
+  playbutton = IMG_Load("play.jpg");
+  Uint32 color_key=SDL_MapRGB(playbutton->format,255,255,255);
+  SDL_SetColorKey(playbutton,SDL_TRUE,color_key);
+  if (playbutton==NULL)    return false;
+  playtext=SDL_CreateTextureFromSurface(renderer,playbutton);
+  if (backtext==NULL)     return false;
+  SDL_RenderCopy(renderer,playtext,NULL,&PlayRect);
+  SDL_RenderPresent(renderer);
+  return true;
+}
+
+bool isOnButton(Uint32 x,Uint32 y){
+  if (x>=357&&x<=564)
+    if (y>=196&&y<=322)    return true;
+  return false;
+}
+
 int main(int argc, char* argv[]){
-  if (! sys_Init())
-    cout << "erreur" << endl;
-  if (!DrawBackGround())
-    cout << "erreur" << endl;
-   SDL_Event windowEvent; // SDL窗口事件
-    while(true) {
-        if (SDL_PollEvent(&windowEvent)) { // 对当前待处理事件进行轮询。
-            if (SDL_QUIT == windowEvent.type) { // 如果事件为推出SDL，结束循环。
-                cout << "SDL quit!!" << endl;
-                break;
-            }
-	    else if(SDL_KEYDOWN  == windowEvent.type ){
-	      //
-	    }
-	    
-        }
-	SDL_RenderCopy(renderer, backtext, NULL, NULL);
-        //显示出来
-        SDL_RenderPresent(renderer);
+  if (! sys_Init())       halt();
+  if (!DrawBackGround())  halt();
+  if (!PutButton())        halt();
+  SDL_Event windowEvent; // SDL窗口事件
+  bool quit=false;
+  while(!quit) {
+    if (SDL_PollEvent(&windowEvent)) { // 对当前待处理事件进行轮询。
+      switch (windowEvent.type) {
+      case SDL_QUIT:       
+	cout << "SDL quit!!" << endl;
+	quit = true;
+	break;
+      case SDL_MOUSEBUTTONDOWN:
+	if (windowEvent.button.button==SDL_BUTTON_LEFT)
+	  if (isOnButton(windowEvent.button.x,windowEvent.button.y))
+	    cout << "is on the play button!"  << endl;
+	break;
+      }
     }
-    SDL_DestroyTexture(backtext);
-    SDL_FreeSurface(background);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window); // 推出SDL窗体
-    SDL_Quit(); // SDL推出
+  }
+  
+  Destroy();
   
   return 0;
   
